3244d1015ab543bcfcf3dd3523e35a3c8f9f78c9
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-04-14 03:31:46 +0000

    added String intrinsic

---
+++ intrinsics/string_t.h
@@ -0,0 +1,45 @@
+#ifndef INTRINSICS_STRING_T_H
+#define INTRINSICS_STRING_T_H
+#include "stdint.h"
+#include "stdlib.h"
+
+
+typedef const uintptr_t ptr_t;
+
+typedef struct {
+    uint8_t size;
+    char* chars;
+} string_t;
+
+
+string_t* allocString(const uint8_t size, const char* cs){
+    // add one to size for the initial byte
+    string_t* ptr = (string_t*)malloc(sizeof(string_t));
+    ptr->size = size;
+    ptr->chars = (char*)calloc(size + 1, 1);
+    for (uint8_t i = 0; i < size; i++)
+        *(ptr->chars + i) = *(cs + i);
+    return ptr;
+}
+
+void freeString(string_t* str){
+    free(str->chars);
+    free(str);
+    return;
+}
+
+string_t* string_concat(string_t* str1, string_t* str2){
+    // add one to new_size for the initial byte
+    string_t* new_str = (string_t*)realloc(str1, str1->size + str2->size + 1);
+    for (uint8_t j = 0; j < str2->size; j++)
+        new_str->chars[str1->size + j] = str2->chars[j];
+    freeString(str2);
+    return new_str;
+}
+
+string_t* chToStr(const char ch){
+    return allocString(1, &ch);
+}
+
+
+#endif
