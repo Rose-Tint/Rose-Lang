439c301bf5f96f6be623855bebac5eee258a32ae
Joe <schubarthjoe7@gmail.com>
2022-05-09 21:25:44 -0500

    added Kind

--- src/Common/Typing/Kind.hs
+++ src/Common/Typing/Kind.hs
@@ -1,13 +1,16 @@
 module Common.Typing.Kind (
     Kind(..),
-    getKind,
+    mkKind,
     kindOf,
+    kindFromList,
+    mkTypeWithKind,
 ) where
 
 import Data.Word(Word8)
 
 import Common.Typing.Constraint
 import Common.Typing.Type
+import Common.Var
 import Pretty
 
 
@@ -16,31 +19,36 @@ default (Word8, Double)
 
 -- | A `Kind` is basically the type of a type.
 -- They work much the same way as haskell's 'kind's.
-newtype Kind = Kind {-# UNPACK #-} Word8
+newtype Kind = Kind Word8
     deriving (Eq)
+
+mkKind :: Integral n => n -> Kind
+mkKind = Kind . fromIntegral
+
+kindFromList :: [a] -> Kind
+kindFromList = mkKind . length
+
+mkTypeWithKind :: Var -> Kind -> Type
+mkTypeWithKind name (Kind k) =
+    Type name (replicate (fromIntegral k) Delayed)
 
 
 instance Pretty Kind where
     pretty (Kind n) = " -> " `seps`
-        replicate (n + 1) "*"
+        replicate (fromIntegral n + 1) "*"
 
 
 class HasKind a where
-    getKind :: a -> Kind
-    {-# INLINABLE getKind #-}
+    kindOf :: a -> Kind
 
 
 instance HasKind Type where
-    getKind (Type _ pars) = Kind
+    kindOf (Type _ pars) = Kind
         (fromIntegral (length pars))
-    getKind (Param pars) = Kind
+    kindOf (Param _ pars) = Kind
         (fromIntegral (length pars))
-    getKind _ = Kind 0
+    kindOf _ = Kind 0
 
-instance HasKind Type where
-    getKind (Constraint _ pars) = Kind
+instance HasKind Constraint where
+    kindOf (Constraint _ pars) = Kind
         (fromIntegral (length pars))
-
-kindOf :: HasKind a => a -> Kind
-{-# INLINE #-}
-kindOf = getKind
