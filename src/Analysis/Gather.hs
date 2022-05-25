d6a4f1e0e0f80253f3ce1933c4006d11317517cc
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-05-25 13:22:17 +0000

    started 'Gather' module

---
+++ src/Analysis/Gather.hs
@@ -0,0 +1,39 @@
+{- |
+THIS MODULE IS NOT TO BE USED YET
+-}
+module Analysis.Gather () where
+
+import Control.Monad.Trans.State
+
+import AST
+import Data.Table
+
+
+-- | Makes a pass to gather all identifiers
+type Gather = StateT Table ()
+
+
+class Gatherable a where
+    gather :: a -> Gather ()
+
+instance Gatherable Pattern where
+    gather (CtorPtrn name args) = do
+        types <- mapM (TypeVar . Var "" . getPos)
+        let retT = TypeVar (Var "" (getPos name))
+            typ = unfoldTypes types retT
+        pushCtor name typ
+    gather (TuplePtrn ptrns) = mapM_ gather ptrns
+    gather _ = return ()
+
+instance Gatherable Value where
+    gather (VarVal name) =
+        pushVar name (TypeVar (Var "" (getPos name)))
+    gather (Application v1 v2) = gather v1 >> gather v2
+
+    | CtorCall Var
+    | Tuple ValArray
+    | Array ValArray
+    | Lambda Var Value
+    | LetIn Var Value Value
+    | IfElseVal Value Value Value
+    | MatchVal Value [ValCase]
