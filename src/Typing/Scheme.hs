f65cf6938e17fbeeddc7758b4075642c752094fd
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-05-25 13:54:25 +0000

    improved 'Pretty' instance for 'Scheme'

--- src/Typing/Scheme.hs
+++ src/Typing/Scheme.hs
@@ -12,11 +12,11 @@ import Typing.Type
 
 -- http://dev.stephendiehl.com/fun/006_hindley_milner.html
 -- haskell example: `forall a b. a -> b`
--- using set-symbols: ∀ab. a -> b
 data Scheme = Forall [Var] Type
 
 
 instance Pretty Scheme where
-    terse (Forall _ typ) = pretty typ
+    terse (Forall _ typ) = terse typ
+    pretty (Forall [] typ) = pretty typ
     pretty (Forall vs typ) = 
         "forall "+|" "`seps`vs|+" . "+|typ
