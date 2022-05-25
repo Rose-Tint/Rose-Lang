99d9d5d009c0d03ad503f0c67e34d648d2ade17b
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-05-25 18:44:07 -0500

    added to 'Utils.Path'

--- src/Utils/Path.hs
+++ src/Utils/Path.hs
@@ -3,10 +3,14 @@ module Utils.Path (
 ) where
 
 
+
+
+
+
 infixr 6 :/
 data Path
     = String :/ Path
-    | Endpoint String
+    | File String String
     deriving (Eq, Ord)
 
 
@@ -15,12 +19,31 @@ infixr 8 //
 (root :/ path) // str = root :/ path // str
 
 fromFilePath :: FilePath -> Path
-fromFilePath "" = Endpoint ""
-fromFilePath ('/':path) = fromFilePath path
-fromFilePath ('\\':path) = fromFilePath path
-fromFilePath path =
-    let (pre, post) = break (`elem` "\\/")
-    in pre :/ fromFilePath post
+fromFilePath "" = rootDir
+fromFilePath str = case parseHome str of
+    Nothing -> go str
+    Just str' -> go str'
+    where
+        go path = case break isDirSep path of
+            (path', []) -> mkFile path'
+            ([], []) -> rootDir
+            (dirName, path') -> dirName :/ go path'
+        parseHome ('~':rest) = Just (dropWhile isDirSep rest)
+        parseHome (c:cs) | isDirSep c = Just (dropWhile isDirSep cs)
+        parseHome _ = Nothing
+
+mkFile :: String -> Path
+mkFile str =
+    let (revExt, revName) = break (== '.') (reverse str)
+    in File (reverse revName) (reverse revExt)
+
+isValidPathChar :: Char -> Bool
+isValidPathChar c = isAlphaNum c || c `elem` "_- ."
+
+isDirSep :: Char -> Bool
+isDirSep '/' = True
+isDirSep '\\' = True
+isDirSep _ = False
 
 toFilePath :: Path -> FilePath
 toFilePath (root :/ path) =
